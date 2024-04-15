38af5f4a20d74653f4eb66d65fe562dc89be97a8
xaviduds <xaviduds@gmail.com>
2024-04-15 17:44:05 -0300

    Appearence, blackEwhite ing

--- flake.nix
+++ flake.nix
@@ -55,7 +55,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-colors.url = "github:Misterio77/nix-colors";
   };
   outputs = { self, nixpkgs, ... }@inputs:
     let
