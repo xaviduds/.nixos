488914f89f3aa6fb25a78652bcbe687c96017fa6
xaviduds <xaviduds@gmail.com>
2024-04-16 15:25:26 -0300

    tmux

--- home.nix
+++ home.nix
@@ -76,115 +76,37 @@ in {
       '';
       ".config/rofi/xaviduds.rasi".text = ''
         * {
-            bg-col:  #1e1e2e;
-            bg-col-light: #1e1e2e;
-            border-col: #1e1e2e;
-            selected-col: #1e1e2e;
-            blue: #89b4fa;
-            fg-col: #cdd6f4;
-            fg-col2: #f38ba8;
-            grey: #6c7086;
-
-            width: 600;
+            width: 600px;
             font: "JetBrainsMono Nerd Font 14";
-        }
-
-        element-text, element-icon , mode-switcher {
-            background-color: inherit;
-            text-color:       inherit;
+            text-color: #${white};
+            background-color: #${black};
         }
 
         window {
-            height: 360px;
+            height: 600px;
             border: 3px;
-            border-color: @border-col;
-            background-color: @bg-col;
-        }
-
-        mainbox {
-            background-color: @bg-col;
+            border-color: #${white};
         }
 
         inputbar {
             children: [prompt,entry];
-            background-color: @bg-col;
             border-radius: 5px;
             padding: 2px;
         }
 
         prompt {
-            background-color: @blue;
             padding: 6px;
-            text-color: @bg-col;
             border-radius: 3px;
             margin: 20px 0px 0px 20px;
-        }
-
-        textbox-prompt-colon {
-            expand: false;
-            str: ":";
         }
 
         entry {
             padding: 6px;
             margin: 20px 0px 0px 10px;
-            text-color: @fg-col;
-            background-color: @bg-col;
-        }
-
-        listview {
-            border: 0px 0px 0px;
-            padding: 6px 0px 0px;
-            margin: 10px 0px 0px 20px;
-            columns: 2;
-            lines: 5;
-            background-color: @bg-col;
         }
 
         element {
             padding: 5px;
-            background-color: @bg-col;
-            text-color: @fg-col  ;
-        }
-
-        element-icon {
-            size: 25px;
-        }
-
-        element selected {
-            background-color:  @selected-col ;
-            text-color: @fg-col2  ;
-        }
-
-        mode-switcher {
-            spacing: 0;
-          }
-
-        button {
-            padding: 10px;
-            background-color: @bg-col-light;
-            text-color: @grey;
-            vertical-align: 0.5; 
-            horizontal-align: 0.5;
-        }
-
-        button selected {
-          background-color: @bg-col;
-          text-color: @blue;
-        }
-
-        message {
-            background-color: @bg-col-light;
-            margin: 2px;
-            padding: 2px;
-            border-radius: 5px;
-        }
-
-        textbox {
-            padding: 6px;
-            margin: 20px 0px 0px 20px;
-            text-color: @blue;
-            background-color: @bg-col-light;
         }
       '';
     };
@@ -698,6 +620,11 @@ in {
         bind-key k  select-pane -U
         bind-key l  select-pane -R
 
+        bind-key C-h  resize-pane -L 5
+        bind-key C-j  resize-pane -D 5
+        bind-key C-k  resize-pane -U 5
+        bind-key C-l  resize-pane -R 5
+
         set -g @plugin 'tmux-plugins/tpm'
         set -g @plugin 'tmux-plugins/tmux-resurrect'
 
@@ -715,7 +642,7 @@ in {
     # ⠀⠀⠀⠀⠀⠀⠀⢀⡼⢿⡷⣾⠀⢀⡞⠁⠀⠹⡄⢻⣿⣿⡆⠀⠘⣿⣦⣤⣀⣀⠀⠀⣀⣀⣤⣶⣿⡯⠀⢀⣾⣿⡿⠋⢀⡞⠀⠀⠙⢆⣀⣿⣻⣯⣷⡀⠀⠀⠀⠀⠀
     # ⠀⠀⠀⠀⠀⠀⢀⡾⠷⠛⢳⡞⣻⠋⠀⠀⠀⠀⢳⡀⢻⣿⣿⣦⣠⣿⡯⣷⡉⣽⠿⠿⠟⠉⣹⡯⣿⣷⣤⣾⣿⣿⠁⠀⣼⠃⠀⠀⠀⠈⠻⡍⠏⠁⠉⢷⡀⠀⠀⠀⠀
     # ⠀⠀⠀⠀⠀⢀⡾⠁⠀⠀⠈⡱⠁⠀⠀⠀⠀⠀⠈⢷⠈⣿⣿⣿⠟⣿⣇⡝⣮⡈⠀⠀⠀⣴⡟⠀⢿⡟⢿⣻⣿⣇⠀⣰⠇⠀⠀⠀⠀⠀⠀⠙⡄⠀⠀⠀⢿⡀⠀⠀⠀
-    # ⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⡼⠃⠀⠀⠀⣀⣀⣠⣤⣼⠿⣿⢿⠃⣰⠋⠈⠁⢻⡙⢶⣴⠟⣹⠃⠀⠀⠱⡄⢹⣿⣟⠲⢿⠤⠤⣤⣤⣀⡀⠀⠀⠸⡆⠀⠀⠈⣧⠀⠀⠀<img align="right" width="100" src="URL_OF_YOUR_IMAGE">
+    # ⠀⠀⠀⠀⠀⣼⠇⠀⠀⠀⡼⠃⠀⠀⠀⣀⣀⣠⣤⣼⠿⣿⢿⠃⣰⠋⠈⠁⢻⡙⢶⣴⠟⣹⠃⠀⠀⠱⡄⢹⣿⣟⠲⢿⠤⠤⣤⣤⣀⡀⠀⠀⠸⡆⠀⠀⠈⣧⠀⠀⠀
     # ⠀⠀⠀⠀⢠⡏⠀⠀⠀⣰⣃⣤⠖⠋⣉⡡⢆⣠⠟⠁⣼⣿⡿⢸⣇⠶⠊⠀⢸⣷⠛⠉⢳⣿⠀⠀⠐⢶⠹⡌⣿⡿⣆⠈⠱⢦⡐⠦⣄⣉⣙⣶⣄⣹⡀⠀⠀⢸⡇⠀⠀
     # ⠀⠀⠀⠀⣾⠀⠀⢰⣶⣿⣿⣿⡿⢿⣥⣶⣟⣁⣠⣞⣽⠟⡇⢸⣿⡀⣀⡴⠋⢹⡄⠀⣸⣉⣻⣦⣄⣸⣰⡇⣿⢹⣮⣷⣤⣤⣿⠿⠞⣛⣿⣿⣿⣿⡿⠂⠀⠀⢿⠀⠀
     # ⠀⠀⠀⠀⡟⠀⠀⠀⢹⠋⠳⢿⣿⣷⡶⠦⢭⣽⣾⣿⡟⠰⢷⣘⣿⠁⠿⠋⠉⠙⣿⠉⡿⠉⠉⠉⠏⢩⣿⢠⣟⣐⣿⣿⢷⣾⣷⣒⣩⣿⠿⠟⠉⠀⢱⠀⠀⠀⢸⠀⠀
