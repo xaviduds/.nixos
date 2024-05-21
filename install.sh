5d5b4b4d1854ff7275f4936dd299a808a2f096d0
xaviduds <xaviduds@gmail.com>
2024-05-20 23:47:36 -0300

    added stuff to install script

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 #ahahah
+
+git clone git@github.com:xaviduds/.nixos.git
+
+git clone git@github.com:xaviduds/xaviduds.github.io.git
+
+git clone git@github.com:xaviduds/.lincePessoal.git
+
+git clone git@github.com:lince-social/lince.git
