f668d6319d8a68b5e2f99d9a779fa27cf345980a
xaviduds <xaviduds@gmail.com>
2024-05-20 23:49:35 -0300

    added path to cloning gitrepos

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 #ahahah
 
-git clone git@github.com:xaviduds/.nixos.git
+git clone git@github.com:xaviduds/.nixos.git ~/
 
-git clone git@github.com:xaviduds/xaviduds.github.io.git
+git clone git@github.com:xaviduds/xaviduds.github.io.git ~/
 
-git clone git@github.com:xaviduds/.lincePessoal.git
+git clone git@github.com:xaviduds/.lincePessoal.git ~/
 
-git clone git@github.com:lince-social/lince.git
+git clone git@github.com:lince-social/lince.git ~/
