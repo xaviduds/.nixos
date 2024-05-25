3232831c4a3e18a6db59e1b02584c83e1837fac4
xaviduds <xaviduds@gmail.com>
2024-05-25 19:58:37 -0300

    cleaning etc/nixos e colord

--- configuration.nix
+++ configuration.nix
@@ -49,18 +49,11 @@
   environment.persistence."/persist/system" = {
     hideMounts = true;
     directories = [
-      "/etc/nixos"
       "/var/log"
       "/var/lib/bluetooth"
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
-      {
-        directory = "/var/lib/colord";
-        user = "colord";
-        group = "colord";
-        mode = "u=rwx,g=rx,o=";
-      }
     ];
     files = [ ];
   };
