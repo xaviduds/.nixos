23a7a9f1b5cef051c3239acebc79918e36055930
xaviduds <xaviduds@gmail.com>
2024-05-25 19:57:37 -0300

    add case de config

--- install.sh
+++ install.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-#ahahah
+read -p """
+[0] Installation with disko  / = 250G + swap = 16G.
+[1] Configuring after installation
+""" -r answer
 
-git clone git@github.com:xaviduds/.nixos.git ~/
+case $answer in  
 
-git clone git@github.com:xaviduds/xaviduds.github.io.git ~/
+0)
+;;
 
-git clone git@github.com:xaviduds/.lincePessoal.git ~/
+1)
 
-git clone git@github.com:lince-social/lince.git ~/
+read -p """
+[0] SSH GitHub config
+[1] Configuring after installation
+""" -r answer
+
+case $answer in  
+
+  ssh-keygen -t ed25519 -C 'xaviduds@gmail.com'
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_ed25519
+  cat ~/.ssh/id_ed25519.pub
+
+  git clone git@github.com:xaviduds/.nixos.git ~/.nixos
+  git clone git@github.com:xaviduds/xaviduds.github.io.git ~/
+  git clone git@github.com:xaviduds/.lincePessoal.git ~/
+  git clone git@github.com:lince-social/lince.git ~/
+;;
+
+*)
+  echo "Invalid option selected."
+  exit 1
+;;
+esac
