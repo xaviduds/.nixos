e2612524829f02ee457ac42f66c4d0da4fc24eea
xaviduds <xaviduds@gmail.com>
2024-05-27 23:15:24 -0300

    mudanças uhuuuul v0.2, e contando...

--- installation/install.sh
+++ installation/install.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 
 sudo su
-
-while true; do
-read -p """
-[0] Quit.
+ read -pr "NixOS install script.
 [1] Partitioning with disko and nixos install with impermanence.
 [2] Configuring after install and reboot
-
-Your answer: """ -r answer
-
-case $answer in  
-0) exit 1 ;;
-
-1) read -p """
-Swap is 16G, how much of the drive will NixOS take?
-[0] Quit.
-[1] 250G for /.
-[2] 100% for /.
-
-Your answer: """ -r answer
+Your answer: " answer
 
 case $answer in  
-  0) exit 1 ;;
-  1) curl https://raw.githubusercontent.com/xaviduds/.nixos/main/first_install/disko_dual_boot.nix -o /tmp/disko.nix ;;
-  2) curl https://raw.githubusercontent.com/xaviduds/.nixos/main/first_install/disko_full_linux.nix -o /tmp/disko.nix ;;
-  *) echo "Invalid option selected." ;; esac
-
+1) curl https://raw.githubusercontent.com/xaviduds/.nixos/main/disko.nix -o /tmp/disko.nix
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/nvme0n1"'
 sudo nixos-generate-config --no-filesystems --root /mnt
 sudo mv /tmp/disko.nix /mnt/etc/nixos/
@@ -36,29 +17,13 @@ sudo cp -r /mnt/etc/nixos /persist
 sudo nixos-install --root /mnt --flake /mnt/etc/nixos#default
 reboot ;;
 
-2)
-sudo rm -rf /etc/nixos/*
-ssh-keygen -t ed25519 -C 'xaviduds@gmail.com' && eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519
-
+2) sudo rm -rf /etc/nixos/*
+ssh-keygen -t ed25519 -C 'xaviduds@gmail.com' && eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && cat ~/.ssh/id_ed25519.pub
 while true; do
-cat ~/.ssh/id_ed25519.pub
-
-read -p """
-Have you registered the key?
-[0] Quit.
-[1] Yes.
-
-Your answer: """ -r answer
-
+read -pr "Have you registered the key (y/n)? " answer
 case $answer in  
-  0) exit 1 ;;
-  1) git clone git@github.com:xaviduds/.nixos.git ~/.nixos
+  y) git clone git@github.com:xaviduds/.nixos.git ~/.nixos
   git clone git@github.com:xaviduds/xaviduds.github.io.git ~/xaviduds.github.io
   git clone git@github.com:xaviduds/.lince_pessoal.git ~/.lince_pessoal
-  git clone git@github.com:lince-social/lince.git ~/lince ;;
-  *) echo "Invalid option selected." ;; esac done
-
-sudo nix flake update ~/.nixos/ && sudo nixos-rebuild switch --flake ~/.nixos#default --impure
-reboot ;;
-
-*) echo "Invalid option selected." ;; esac done
+  git clone git@github.com:lince-social/lince.git ~/lince ;; esac done
+sudo nix flake update ~/.nixos/ && sudo nixos-rebuild switch --flake ~/.nixos#default --impure && reboot ;; esac
