ef0f4b6219f4d010dcc85f37db1c485935557b57
xaviduds <xaviduds@gmail.com>
2024-06-01 18:47:38 -0300

    script trying

--- installation/install.sh
+++ installation/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-read -p "NixOS Install script, what do you want? [I]nstall or [C]lone repos and build: " -r answer
+echo "NixOS Install script, what do you want?"
+read -p "[I]nstall | After-install and [S]SH | [C]lone repos and build: " -r answer
 
 case $answer in
 I)
@@ -12,13 +13,14 @@ I)
     curl https://raw.githubusercontent.com/xaviduds/.nixos/main/installation/flake.nix -o /mnt/etc/nixos/flake.nix
     curl https://raw.githubusercontent.com/xaviduds/.nixos/main/installation/configuration.nix -o /mnt/etc/nixos/configuration.nix
     cp -r /mnt/etc/nixos /mnt/persist
-    nixos-install --root /mnt --flake /mnt/etc/nixos#default
+    nixos-install --root /mnt --flake /mnt/etc/nixos#default && reboot ;;
+S)
     sudo rm -rf /etc/nixos/*
     ssh-keygen -t ed25519 -C 'xaviduds@gmail.com' && eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && cat ~/.ssh/id_ed25519.pub ;;
 C)
-          git clone git@github.com:xaviduds/.nixos.git /home/nixos/.nixos
-          git clone git@github.com:xaviduds/xaviduds.github.io.git /home/nixos/xaviduds.github.io
-          git clone git@github.com:xaviduds/.lince_pessoal.git /home/nixos/.lince_pessoal
-          git clone git@github.com:lince-social/lince.git /home/nixos/lince
-        nix flake --extra-experimental-features "nix-command flakes" update ~/.nixos/ && nixos-rebuild boot --flake ~/.nixos#default--impure && reboot ;;
+    git clone git@github.com:xaviduds/.nixos.git /home/eduardo/.nixos
+    git clone git@github.com:xaviduds/xaviduds.github.io.git /home/eduardo/xaviduds.github.io
+    git clone git@github.com:xaviduds/.lince_pessoal.git /home/eduardo/.lince_pessoal
+    git clone git@github.com:lince-social/lince.git /home/eduardo/lince
+    nix flake --extra-experimental-features "nix-command flakes" update ~/.nixos/ && export NIXPKGS_ALLOW_UNFREE=1 && nixos-rebuild boot --flake ~/.nixos#default --impure && reboot ;;
 esac
