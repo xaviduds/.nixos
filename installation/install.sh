c66bfb03ddeab4e987f549bd31ee26193de684c4
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-06-03 18:50:54 +0000

    uu

--- installation/install.sh
+++ installation/install.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
 while true; do
-
-echo "NixOS Install script, what do you want?"
 read -p "[I]nstall | [R]ebuild, [S]SH GitHub config | Repo [C]loning: " -r answer
-
 case $answer in
 I)
-    echo "Partitioning with disko and nixos install with impermanence..."
+    # Pull install config files
     sudo curl -L https://raw.githubusercontent.com/xaviduds/.nixos/main/disko.nix -o /tmp/disko.nix
+    sudo curl -L https://raw.githubusercontent.com/xaviduds/.nixos/main/installation/flake.nix -o /tmp/flake.nix
+    sudo curl -L https://raw.githubusercontent.com/xaviduds/.nixos/main/installation/configuration.nix -o /tmp/configuration.nix
+
+    # Scheme
     sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/nvme0n1"'
     sudo nixos-generate-config --no-filesystems --root /mnt
-    sudo mv /tmp/disko.nix /mnt/etc/nixos/
-    # sudo nix flake init --extra-experimental-features "nix-command flakes"
-    sudo curl -L https://raw.githubusercontent.com/xaviduds/.nixos/main/installation/flake.nix -o /mnt/etc/nixos/flake.nix
-    sudo curl -L https://raw.githubusercontent.com/xaviduds/.nixos/main/installation/configuration.nix -o /mnt/etc/nixos/configuration.nix
-    sudo cp -r /mnt/etc/nixos /mnt/persist
-    sudo nixos-install --root /mnt --flake /mnt/etc/nixos#default && reboot
+
+    # mv install config files to it`s place
+    sudo mv /tmp/*.nix /mnt/etc/nixos/ && sudo mv /mnt/etc/nixos /mnt/persist
+
+    # Install
+    sudo nixos-install --root /mnt --flake /mnt/persist/nixos#default
+
+    # Clone real repo to a persistent dir and reboot
+    git clone https://github.com/xaviduds/.nixos.git /mnt/persist/.nixos && reboot
     ;;
 R)
-    sudo rm -rf /etc/nixos/*
-    git clone https://github.com/xaviduds/.nixos.git ~/.nixos
-    sudo nix flake --extra-experimental-features "nix-command flakes" update ~/.nixos/ && export NIXPKGS_ALLOW_UNFREE=1 && sudo nixos-rebuild boot --flake ~/.nixos#default --impure
-    reboot
+    sudo cp -r /persist/.nixos ~/.nixos && mv ~/.nixos/installation/home_second_boot.nix ~/.nixos/home.nix
+    sudo nixos-rebuild boot --flake ~/.nixos#default && reboot
     ;;
 S)
     ssh-keygen -t ed25519 -C 'xaviduds@gmail.com' && eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && cat ~/.ssh/id_ed25519.pub
@@ -35,5 +37,4 @@ C)
     git clone git@github.com:lince-social/lince.git /home/eduardo/lince
     ;;
 esac
-
 done
