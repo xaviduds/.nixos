c61576c7528f39a87c505abfc179ebe715e82eff
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-06-03 16:44:25 +0000

    add wayland

--- installation/configuration.nix
+++ installation/configuration.nix
@@ -38,7 +38,7 @@
 
   services = {
     openssh.enable = true;
-      displayManager.sddm.enable = true;
+      displayManager.sddm.wayland.enable = true;
       desktopManager.plasma6.enable = true;
   };
 
