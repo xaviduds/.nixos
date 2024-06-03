023a684ef2cee03191a576083b719969dd6bb2ad
Eduardo de Melo Xavier <xaviduds@gmail.com>
2024-06-03 15:19:27 +0000

    testing

--- installation/install.sh
+++ installation/install.sh
@@ -3,7 +3,7 @@
 while true; do
 
 echo "NixOS Install script, what do you want?"
-read -p "[I]nstall | After-install and [S]SH | [C]lone repos and build: " -r answer
+read -p "[I]nstall | [R]ebuild, [S]SH GitHub config | Repo [C]loning: " -r answer
 
 case $answer in
 I)
@@ -12,14 +12,19 @@ I)
     sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/nvme0n1"'
     sudo nixos-generate-config --no-filesystems --root /mnt
     sudo mv /tmp/disko.nix /mnt/etc/nixos/
-    sudo nix flake init --template github:vimjoyer/impermanent-setup --extra-experimental-features "nix-command flakes"
+    sudo nix flake init --template github:xaviduds/.nixos/installation --extra-experimental-features "nix-command flakes"
     sudo curl https://raw.githubusercontent.com/xaviduds/.nixos/main/installation/flake.nix -o /mnt/etc/nixos/flake.nix
     sudo curl https://raw.githubusercontent.com/xaviduds/.nixos/main/installation/configuration.nix -o /mnt/etc/nixos/configuration.nix
     sudo cp -r /mnt/etc/nixos /mnt/persist
     sudo nixos-install --root /mnt --flake /mnt/etc/nixos#default && reboot
     ;;
-S)
+R)
     sudo rm -rf /etc/nixos/*
+    git clone https://github.com/xaviduds/.nixos.git ~/.nixos
+    sudo nix flake --extra-experimental-features "nix-command flakes" update ~/.nixos/ && export NIXPKGS_ALLOW_UNFREE=1 && sudo nixos-rebuild boot --flake ~/.nixos#default --impure
+    reboot
+    ;;
+S)
     ssh-keygen -t ed25519 -C 'xaviduds@gmail.com' && eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519 && cat ~/.ssh/id_ed25519.pub
     gh auth login
     ;;
@@ -28,8 +33,6 @@ C)
     git clone git@github.com:xaviduds/xaviduds.github.io.git /home/eduardo/xaviduds.github.io
     git clone git@github.com:xaviduds/.lince_pessoal.git /home/eduardo/.lince_pessoal
     git clone git@github.com:lince-social/lince.git /home/eduardo/lince
-    sudo nix flake --extra-experimental-features "nix-command flakes" update ~/.nixos/ && export NIXPKGS_ALLOW_UNFREE=1 && sudo nixos-rebuild boot --flake ~/.nixos#default --impure
-    reboot
     ;;
 esac
 
